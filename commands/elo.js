045d99a07887d28407c47c8ac044ad912d741944
github_username <rpswanson18@gmail.com>
2020-12-03 21:34:07 -0800

    test

---
+++ commands/elo.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'elo',
+    description: 'Pong!',
+    execute(msg, args) {
+        msg.channel.send(getNewRating(1600, 1700 ,1));
+    },
+};
+
+
+function getRatingDelta(myRating, opponentRating, myGameResult) {
+    if ([0, 0.5, 1].indexOf(myGameResult) === -1) {
+        return null;
+    }
+
+    let myChanceToWin = 1 / ( 1 + Math.pow(10, (opponentRating - myRating) / 400));
+
+    return Math.round(32 * (myGameResult - myChanceToWin));
+}
+
+function getNewRating(myRating, opponentRating, myGameResult) {
+    return myRating + getRatingDelta(myRating, opponentRating, myGameResult);
+}
